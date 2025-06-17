30b8f14484a9f358cf5c3249ef2b3acfe3371ea2
Assil <assilmahfoudi@gmail.com>
2025-06-17 10:27:17 +0200

    commit with error

--- test_math_ops.c
+++ test_math_ops.c
@@ -3,7 +3,7 @@
 
 MU_TEST(test_add_2_numbers_int) {
     mu_assert(add_2_numbers_int(2, 3) == 5, "2 + 3 should be 5");
-    mu_assert(add_2_numbers_int(-1, 1) == 0, "-1 + 1 should be 0");
+    mu_assert(add_2_numbers_int(-1, 1) == 1, "-1 + 1 should be 0");
     mu_assert(add_2_numbers_int(0, 0) == 0, "0 + 0 should be 0");
     mu_assert(add_2_numbers_int(-5, -7) == -12, "-5 + -7 should be -12");
 }
