4ed3a88f730d167cbb0511bc5dbd0705f03e2fb2
baptistegdb <baptiste.gadebille@ynov.com>
2025-06-19 14:22:00 +0200

    start.sh updated

--- start.sh
+++ start.sh
@@ -1,20 +1,32 @@
-# Define a cleanup function to be executed when the script is interrupted (with Ctrl+C)
+#!/bin/bash
+
 cleanup(){
-    # In an activated virtual environment
     python -m space_collector.killall
 }
 
+# Kill le client et le serveur localhost de space_collector
+python -m space_collector.killall
+
 trap cleanup SIGINT
 
-# choose a free port
 # choisir un port non utilisé
 PORT=$((RANDOM % 1001 + 2000))
-# start the server
-python -m space_collector.game.server -p $PORT --timeout 5 &
-# start the viewer on small screens
-python -m space_collector.viewer -p $PORT --small-window&
-# start players
-# using serial port
-# SERIAL=COM8          # on Windows
-SERIAL=/dev/ttyUSB0  # on Linux
+
+# Démarrer le serveur
+python -m space_collector.game.server -p $PORT --timeout 4 &
+SERVER_PID=$!
+
+# Démarrer le viewer
+python -m space_collector.viewer -p $PORT --small-window &
+VIEWER_PID=$!
+
+# Attendre que le serveur soit prêt
+echo "En attente que le serveur écoute sur le port $PORT..."
+while ! nc -z localhost $PORT; do
+  sleep 0.5
+done
+
+# Démarrer le client serial2tcp
+SERIAL=/dev/ttyUSB0  # sur Linux
 python -m space_collector.serial2tcp -p $PORT --serial $SERIAL --team-name "CHANGE ME"
+
